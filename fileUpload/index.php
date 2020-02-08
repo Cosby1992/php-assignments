8dccdc496a22b37f8ada7c225d07837de9fed5d7
Cosby1992 <Ande016j@edu.easj.dk>
2020-02-08 23:52:37 +0100

    File upload style update.

--- fileUpload/index.php
+++ fileUpload/index.php
@@ -4,26 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./style.css">
     <title>Fileupload</title>
 </head>
 <body>
 
-    <?php 
-
-        // Writes error to user if one is set in get request
-        if (isset($_GET['err'])){
-            echo '<h1 class="header-text">' . $_GET['err'] . '</h1>';
-        } else {
-            echo "<h1>The Upload page!</h1>";
-        }
-    ?>
-
+    <div class="container">
     
-    <!-- Form that takes a file and allows user to submit file to upload -->
-    <form action="/upload.php" method="post" enctype="multipart/form-data">
-        <input type="file" name="fileUploaded" id="inputFileUpload">
-        <button type="submit" name="submit" id="btn_uplaod_submit">Upload Fil</button>
-    </form>
+        <?php 
+
+            // Writes error to user if one is set in get request
+            if (isset($_GET['err'])) {
+                echo '<h1 class="header-text">' . $_GET['err'] . '</h1>';
+            } else {
+                echo '<h1 class="header-text">The Upload page!</h1>';
+            }
+        ?>
+
+        
+        <!-- Form that takes a file and allows user to submit file to upload -->
+        <form action="/upload.php" method="post" enctype="multipart/form-data">
+            <input type="file" name="fileUploaded" id="inputFileUpload">
+            <label for="inputFileUpload" id="chooseFileLabel">Vælg fil</label>
+            <button type="submit" name="submit" id="btn_uplaod_submit">Upload Fil</button>
+        </form>
+
+    </div>
+
+
     
 </body>
 </html>
