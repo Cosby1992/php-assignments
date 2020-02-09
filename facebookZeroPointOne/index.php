ceb7d7e00227ddd6583690b77302d5965fc260af
Cosby1992 <ande016j@edu.easj.dk>
2020-02-09 18:22:46 +0100

    style update and form update in index.php

--- facebookZeroPointOne/index.php
+++ facebookZeroPointOne/index.php
@@ -5,51 +5,63 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>No Face Chat</title>
 
-    <link rel="stylesheet" href="faceStyle.css">
-    <link rel="stylesheet" href="message.css">
+    <!-- // Import of stylesheets -->
+    <link rel="stylesheet" href="./styles/faceStyle.css">
+    <link rel="stylesheet" href="./styles/message.css">
 
     <!-- Bootstrap css -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 <body>
 
-<div class="container">
+    <div class="container">
 
+        <div class="container-sm">
 
-    <div class="container-sm">
+            <h1>No Face Chat</h1>
 
-    <h1>No Face Chat</h1>
+            <!-- Form that posts on sendmsg.php, that updates db -->
+            <div class="form-cont">
+                <form action="/sendmsg.php" method="post">
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Write here!" aria-label="Message" aria-describedby="basic-addon2" name="msg">
+                        <div class="input-group-append">
+                            <button class="btn btn-outline-secondary" type="submit" id="button-addon2">►</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
 
-        <div class="msgscrollview">
+            <!-- contains scroll view and messages -->
+            <div class="msgscrollview">
 
-            <?php
-                include './database/database.php';
+                <?php
+                    // include database php
+                    include './database/database.php';
 
-                getMessages();
+                    // get messages from database
+                    getMessages();
 
-            ?>
+                ?>
 
+            </div>
+
+            <!-- first attemp
+            <div id="sendTextContainer">
+                <form action="/sendmsg.php" method="post">
+                    <input type="text" class="form-control" name="msg" id="msginput">
+                    <button type="submit" class="btn btn-primary" id="sendbtn">Send</button>
+                </form>
+            </div>
+            -->
+            
         </div>
-
-        
     </div>
 
-    <div id="sendTextContainer">
-        <form action="/sendmsg.php" method="post">
-            <input type="text" class="form-control" name="msg" id="msginput">
-            <button type="submit" class="btn btn-primary" id="sendbtn">Send</button>
-        </form>
-    </div>
-
-
-</div>
-
-    
-
-<!-- Bootstrap js -->
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <!-- Bootstrap js -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
 </body>
 </html>
