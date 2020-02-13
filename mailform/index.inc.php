40e972a190fe5825907e596dd24283ea499a520e
Cosby1992 <Ande016j@edu.easj.dk>
2020-02-13 23:21:36 +0100

    Mailform initial commit.

---
+++ mailform/index.inc.php
@@ -0,0 +1,8 @@
+
+<?php
+
+ //import code
+
+
+
+?>
---
+++ mailform/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mailform</title>
+
+    <link rel="stylesheet" href="global.css">
+</head>
+<body>
+
+    <?php
+
+        include 'index.inc.php';
+        //Server stuff here
+
+    ?>
+
+    <h1>Mailform</h1>
+    <p>Welcome to Mailform</p>
+    
+</body>
+</html>
