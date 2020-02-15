01a80a634f434b23115d85062c6546e1b81b8888
Cosby1992 <Ande016j@edu.easj.dk>
2020-02-15 11:39:59 +0100

    fav icon and email function added. Not working due to google credentials.

--- mailform/README.md
+++ mailform/README.md
@@ -1,1 +1,10 @@
-# Content under develobment 
+# Google would not allow my credentials to be used with this app
+
+It required setup of sendmail.ini and php.ini to send emails. 
+With that done, google wouldn't accept my credentials to be used with a non-google app. 
+
+-----------------------------------------------------------------------------------------------
+ 
+So close
+ - Cosby 
+    
--- mailform/index.inc.php
+++ mailform/index.inc.php
@@ -3,6 +3,21 @@
 
  //import code
 
+ function sendEmail(){
+    $to = "example@exapmle.com";
+    $subject = "Mail from php";
+    $txt = "My php mail service is running!";
+
+    // Always set content-type when sending HTML email
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    $headers .= "From: webmaster@example.com" . "\r\n" .
+    "CC: somebodyelse@example.com";
+
+    return mail($to,$subject,$txt,$headers);
+ }
+
+
 
 
 ?>
